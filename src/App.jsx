fb938b248e6994891d020139a5241bf655abe89c
anuragsayshelloworld <anuragsayshelloworld@gmail.com>
2025-05-25 09:06:17 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Login from './components/Login';
+import AdminDashboard from './components/AdminDashboard';
+export default function App(){
+  return(
+    <>
+    <p>One day! Game finishes!</p> 
+    <Login />
+    <AdminDashboard />
+    </>
+    );
+}
---
+++ src/components/AdminDashboard.jsx
@@ -0,0 +1,4 @@
+export default function AdminDashboard(){
+
+	return(<p>hello hello</p>);
+}
