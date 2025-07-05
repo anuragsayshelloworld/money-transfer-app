d7423cb8908e90abfeaab9650116c6291ffea8fa
anuragsayshelloworld <anuragsayshelloworld@gmail.com>
2025-07-05 19:52:47 +0545

    removed most old code

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { LoginProvider } from './context/LoginContext'; // ✅ Use this
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LoginProvider>
       <App />
-    </LoginProvider>
   </React.StrictMode>
 );
